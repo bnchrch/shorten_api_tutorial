c7b9259fac21ce8496e38e72fcc0c7b4a3633dea
Ben Church <ben.church@metalab.co>
2018-08-06 01:28:18 -0700

    Generate a unique id for each url

--- lib/shorten_api/links/link.ex
+++ lib/shorten_api/links/link.ex
@@ -2,6 +2,12 @@ defmodule ShortenApi.Links.Link do
   use Ecto.Schema
   import Ecto.Changeset
 
+  # TODO
+  # - make hashid auto gen
+  # - check for exisitng url
+  # - look up limits for mnesia
+  # setup auth
+  # setup login
   @primary_key {:hash, :string, []}
   @derive {Phoenix.Param, key: :hash}
   schema "links" do
@@ -10,10 +16,22 @@ defmodule ShortenApi.Links.Link do
     timestamps()
   end
 
+  def random_string(length) do
+    length
+    |> :crypto.strong_rand_bytes()
+    |> Base.url_encode64
+    |> binary_part(0, length)
+  end
+
+  defp generate_hash_id(%Ecto.Changeset{changes: %{url: url}} = changeset) do
+    put_change(changeset, :hash, random_string(6))
+  end
+
   @doc false
   def changeset(link, attrs) do
     link
     |> cast(attrs, [:hash, :url])
+    |> generate_hash_id()
     |> validate_required([:hash, :url])
     |> unique_constraint(:hash)
     |> unique_constraint(:url)
